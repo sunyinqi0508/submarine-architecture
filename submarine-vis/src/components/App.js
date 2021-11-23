30576e1e4379e64418b33eb27bfe3756538996e2
BillSun <bill@billsuns-mbp.local>
2021-11-23 15:43:32 -0500

    Visualization: make sure webui can start before game server

--- submarine-vis/src/components/App.js
+++ submarine-vis/src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Websocket from 'react-websocket';
 import { Donut } from './Donut.react';
 import { Submarine, Probe } from './Submarine.react';
 import StatsTable from './StatsTable.react';
@@ -10,7 +9,13 @@ import '../assets/css/App.css';
 class App extends Component {
   constructor() {
     super();
-
+    window.app = this;
+    window.connect = () =>{
+      window.app.ws = new WebSocket('ws://localhost:8000');
+      window.app.ws.onmessage = (e)=>{window.app.handleData(e.data);};
+      window.app.ws.onclose = ()=>{setTimeout(function() {window.connect();}, 1000) };
+    }
+    window.connect();
     this.state = {
       redRegion: 5,
       position: 0,
@@ -63,10 +68,8 @@ class App extends Component {
           <Submarine pos={this.state.position}/>
           {this.state.probes.map(position => <Probe pos={position}/>)}
           <StatsTable {...this.state}/>
-          <Websocket
-            url='ws://localhost:8000'
-            onMessage={this.handleData.bind(this)}
-          />
+
+
         </div>
       </div>
     );
